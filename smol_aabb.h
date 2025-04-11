804e571cb80b00ccd454e47b7afed50f62bddbd5
Antônio Ivo da Silva Gomes <144092212+antonio-ivo@users.noreply.github.com>
2025-04-11 15:03:55 -0300

    update smol_aabb_intersects function
    - update correction about edge touchs

--- smol_aabb.h
+++ smol_aabb.h
@@ -21,8 +21,8 @@ int smol_aabb_intersects(smol_aabb_rect a, smol_aabb_rect b);
 #ifdef SMOL_AABB_IMPLEMENTATION
 
 int smol_aabb_intersects(smol_aabb_rect a, smol_aabb_rect b) {
-  int vertical = a.x + a.w < b.x || b.x + b.w < a.x;
-  int horizontal = a.y + a.h < b.y || b.y + b.w < a.y;
+  int vertical = a.x + a.w <= b.x || b.x + b.w <= a.x;
+  int horizontal = a.y + a.h <= b.y || b.y + b.w <= a.y;
   return !(vertical || horizontal);
 }
 
