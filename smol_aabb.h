3e7c20cc860aed14640e8df9403f11e8e5833e1f
Antônio Ivo da Silva Gomes <144092212+antonio-ivo@users.noreply.github.com>
2025-04-11 15:06:52 -0300

    fix smol_aabb_intersects function
    - correct intersects algorithm

--- smol_aabb.h
+++ smol_aabb.h
@@ -22,7 +22,7 @@ int smol_aabb_intersects(smol_aabb_rect a, smol_aabb_rect b);
 
 int smol_aabb_intersects(smol_aabb_rect a, smol_aabb_rect b) {
   int vertical = a.x + a.w <= b.x || b.x + b.w <= a.x;
-  int horizontal = a.y + a.h <= b.y || b.y + b.w <= a.y;
+  int horizontal = a.y + a.h <= b.y || b.y + b.h <= a.y;
   return !(vertical || horizontal);
 }
 
